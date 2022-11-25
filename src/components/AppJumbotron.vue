b99a95d8d87d1cc1f0548f0571a78ed708fc979e
SaverioCascelli <saverio.cascelli@gmail.com>
2022-11-25 11:40:02 +0100

    [add] badge in jumbontron right

--- src/components/AppJumbotron.vue
+++ src/components/AppJumbotron.vue
@@ -17,11 +17,19 @@ export default {
                     <h3>Online marketing to secure customer retention, lead, and sales. We focus on the bigger picture.</h3>
                     <button class="sc-button bg-white text-black">Our Servicess<i class="fa-solid fa-arrow-right"></i></button>
                 </div>
-            <div class="col-6 right-jumbo">
-                <ctaCard
-                class="jumbo-card"/>
+                <div class="col-6 right-jumbo">
+                    <ctaCard
+                    class="jumbo-card"/>
+                </div>
             </div>
-            </div>
+        </div>
+        <div class="info-box">
+            <i class="top fa-solid fa-folder-open"></i>
+            <p class="bottom">Demos</p>
+        </div>
+        <div class="info-box ">
+            <p class="top col-magic-mint"><span>$</span>39</p>
+            <p class="bottom">On Sale</p>
         </div>
     </div> 
   
@@ -37,6 +45,7 @@ export default {
     background-repeat: no-repeat;
     background-size: 120%;
     background-position: 50% 0%;
+    position: relative;
     
     .left-jumbo{
         padding: 120px 0;
@@ -59,6 +68,43 @@ export default {
             left: 17%;
         }
     }
+
+    .info-box{
+        text-align: center;
+        font-size: .7rem;
+        width: 65px ;
+        aspect-ratio: 1 / 1;
+        position: absolute;
+        right: 10px;
+        top: 30px;
+        background: white;
+        border: 1px solid;
+        border-radius: 10px;
+        padding: 2px;
+        .top{
+            height: 70%;
+            font-size: 1.7rem;
+            padding: 5px;
+            padding-left: 15px;
+            line-height: 1.7rem;
+            font-weight: bold;
+            position: relative
+            }
+        span{
+            font-size: 1rem;
+            display: inline-block;
+            position: absolute;
+            top: -3px;
+            left: 10px;
+            
+        }
+        & > *{
+            margin: 0;
+        }
+        &:last-child{
+            top: 100px;
+        }
+     }
 }
 
 </style>
