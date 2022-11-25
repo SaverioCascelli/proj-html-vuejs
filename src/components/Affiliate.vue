587dfa20a67635fc7bba75f416c66f958aab1939
SaverioCascelli <saverio.cascelli@gmail.com>
2022-11-25 15:31:41 +0100

    [add]affiliate section. In progress..

---
+++ src/components/Affiliate.vue
@@ -0,0 +1,45 @@
+<script>
+export default {
+    name:'Affiliate'
+}
+</script>
+
+<template>
+    <div class="row">
+        <div class="col text-center">
+            <h2>Some Brands We Work With</h2>
+            <p>Lorem ipsum dolor sit amet consectetur.</p>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-2">
+            <img src="../assets/abstract.png" alt="">
+        </div>
+        <div class="col-2">
+            <img src="../assets/cglobal.png" alt="">
+        </div>
+        <div class="col-2">
+            <img src="../assets/next.png" alt="">
+        </div>
+        <div class="col-2">
+            <img src="../assets/hemisferio.png" alt="">
+        </div>
+        <div class="col-2">
+            <img src="../assets/spaces.png" alt="">
+        </div>
+        <div class="col-2">
+            <img src="../assets/digitalbox.png" alt="">
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col ">
+
+        </div>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+@use '../style/general.scss' as *;
+
+</style>
